5a60850d6890762444b1fc449917a4e3bf6f63af
Yusupha Bojang <ybojang98@gmail.com>
2022-02-01 12:48:36 +0000

    fghjkl

---
+++ test1.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+    $note = 2;
+
+    if($note>10){
+
+    
+        echo "Admis";
+    
+    }else{  
+        echo"diangatal fils mo guen si yawwwwww mbammmmmmmmmmmmmmm";
+    }
+      
+
+?>
+
+
+
+</body>
+</html>
